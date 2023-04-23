c774f5a317cb93742e18c8c39f196fea41e28ebc
GabrielBoulos <gabrielboulosss@gmail.com>
2023-04-23 12:20:42 -0300

    versão criada

---
+++ jogo.js
@@ -0,0 +1,84 @@
+var altura = 0
+var largura = 0
+var tempo = 15
+var tempoCriarMosquito = 3000
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'facil'){
+	tempoCriarMosquito = 3000
+} else if (nivel === 'normal'){
+	tempoCriarMosquito = 2000
+} else {
+	tempoCriarMosquito = 1000
+}
+
+function ajustarTamanhoJogo(){
+	altura = window.innerHeight
+	largura = window.innerWidth
+	console.log(altura, largura)
+}
+
+ajustarTamanhoJogo()
+
+var cronometroJogo = setInterval(function(){
+	if (tempo <0){
+		clearInterval(cronometroJogo)
+		clearInterval(apresentarMosquito)
+		window.location.href = 'vitoria.html'
+	} else {
+	document.getElementById('cronometro').innerHTML = tempo
+	tempo -= 1}
+}, 1000)
+
+var vidas = 1
+function posicaoRandomica(){
+	if (document.getElementById('mosquito')){
+		document.getElementById('mosquito').remove()
+		if (vidas>3){
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+		document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+		vidas = vidas + 1
+	}
+	}
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = tamanhoRandomico() + " " + ladoRandomico()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function(){
+		this.remove()
+	}
+	document.body.appendChild(mosquito)
+}
+
+function tamanhoRandomico(){
+	var classe = Math.floor(Math.random() * 3)
+	switch(classe){
+	case 0:
+		return 'mosquito1'
+	case 1:
+		return 'mosquito2'
+	case 2:
+		return 'mosquito3'
+	}
+}
+
+function ladoRandomico(){
+	var classe = Math.floor(Math.random() * 2)
+	switch(classe){
+	case 0:
+		return 'ladoA'
+	case 1:
+		return 'ladoB'
+	}
+}
